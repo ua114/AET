9f79c60bbd08124832dd12277d128b5885fa6d10
OptimusPrime <optimusprime@87uu7.home>
2019-02-04 22:16:09 +0000

    more change

--- compressor_design.m
+++ compressor_design.m
@@ -8,3 +8,7 @@ stages = HP_ratio/rp_stage;
 fprintf('Number of compressor stages is : %d\n', floor(stages) +1);
 fprintf('The last stage will have a pressure ratio of: %f\n',stages - floor(stages));
 
+alpha_1 = 10; %Angle of incidence \\ change later
+beta_1 = 10;
+
+angle_incidence = alpha_1 - beta_1; % Independant Variable
