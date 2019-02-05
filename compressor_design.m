a71685ac0271131646cc02fbd26ac3f877ffd20b
OptimusPrime <optimusprime@87uu7.home>
2019-02-05 09:03:26 +0000

    *changed

--- compressor_design.m
+++ compressor_design.m
@@ -6,7 +6,8 @@ rp_stage = 1.6; % Max pressire rise across a stage
 
 stages = HP_ratio/rp_stage;
 fprintf('Number of compressor stages is : %d\n', floor(stages) +1);
-fprintf('The last stage will have a pressure ratio of: %f\n',stages - floor(stages));
+fprintf('The last stage will have a pressure ratio of: %f\n',stages...
+    - floor(stages));
 
 alpha_1 = 10; %Angle of incidence \\ change later
 beta_1 = 10;
