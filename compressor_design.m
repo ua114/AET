960d0539ab80b0ef3af33a7475b0fed16f859150
ua114 <45734311+ua114@users.noreply.github.com>
2019-02-08 10:42:36 +0000

    Meeting

--- compressor_design.m
+++ compressor_design.m
@@ -2,6 +2,9 @@ clc
 % Define Variables.........
 HP_ratio = 13; % Overall HP pressure ratio
 rp_stage = 1.6; % Max pressire rise across a stage
+Fan_diameter = 0.9582; % in m
+C_a = 170; 
+Core_area = 0.0351; %m^2
 %.................
 
 stages = HP_ratio/rp_stage;
@@ -9,7 +12,3 @@ fprintf('Number of compressor stages is : %d\n', floor(stages) +1);
 fprintf('The last stage will have a pressure ratio of: %f\n',stages...
     - floor(stages));
 
-alpha_1 = 10; %Angle of incidence \\ change later
-beta_1 = 10;
-
-angle_incidence = alpha_1 - beta_1; % Independant Variable
