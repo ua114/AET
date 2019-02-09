a4deb605c9d65b0652e6de7d623890c0588dc24b
AHMAD AMMAR <ahmadammar@ahmads-mbp.home>
2019-02-09 19:45:36 +0000

    Remaking code

--- compressor_design.m
+++ compressor_design.m
@@ -10,16 +10,30 @@ rho_cruise = 0.494; % in kg/m^3
 hub_ratio = 0.6;
 M_tip = 1.4; % Maximum mach number at blade tip
 c_cruise = 310; % Speed of sound at cruise altitude
+%..................
+
+% Air properties 
+R = 287; % Absolute gas constant
+gamma = 1.4;
+P_atm = 0.33; % Stagnation Pressure at 28,000 ft in bar
+P_01 = 2.5 * P_atm; % Inlet stagnation Pressure into the HPC
+T_01 = 232.8; % Stagnation temperature at 28,000 ft ? ground level??
+cp = 1.01; %kJ KgK-1
 %.................
 
-stages = HP_ratio/rp_stage;
-fprintf('Number of compressor stages is : %d\n', floor(stages) +1);
+T_1 = T_01 - C_a^2/(2*cp*1000); % HPC inlet temperature
+P_1 = P_01*(T_1/T_01)^(gamma/(gamma-1)); % HPC inlet pressure
+rho_1 = P_1*10^5/(R*T_1); %HPC inlet density
+%.............
 
-% Calculate Blade Tip Radius....
-r_t = sqrt((m_core)/(rho_cruise*pi*C_a*(1-(hub_ratio)^2)));
-fprintf('Initial compressor blade radius is: %f cm\n', r_t*100);
-%.......
+% Annulus dimensions at entry and exit
+r_t_entry = m_core/(pi*rho_1*C_a*(1-hub_ratio^2)); % Tip radius
+r_r_entry = hub_ratio * r_t_entry; % Root radius
 
-% Calculate Blade tip sped
-N = (M_tip*c_cruise)/(2*pi*r_t);
-fprintf('Rotation speed is : %f\n', N);
+fprintf('Tip radius at entry of the HPC is : %f cm\n', r_t_entry *100);
+
+
+
+
+
+
