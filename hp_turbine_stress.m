8d0e56cbdfaf513713907616767f8763ce5b7776
ua114 <45734311+ua114@users.noreply.github.com>
2019-03-08 18:01:36 +0000

    changes

--- hp_turbine_stress.m
+++ hp_turbine_stress.m
@@ -1,6 +1,14 @@
 clc
 % Define Variables.........
-rev = 431.39;
+rev = 431.39; % rev/s
+N = 43; % Number of blades
+m_blade = 0.1; % kg, Mass of each blade
+r_blade = 22; % mm, blade length
 %................
 
-omega = rev * 2*pi;
+omega = rev * 2*pi; %rad/s
+
+F = m_blade * omega^2 * r_blade; % N, force exerted by each blade on the disc
+fprintf('Force exerted by each blade on the disc: %1.0f kN\n', F/1000);
+
+
