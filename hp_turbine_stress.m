3ecd4360f167d55c5033992c2a106d0f97cf75df
ua114 <45734311+ua114@users.noreply.github.com>
2019-03-08 18:12:26 +0000

    end of day

--- hp_turbine_stress.m
+++ hp_turbine_stress.m
@@ -4,11 +4,17 @@ rev = 431.39; % rev/s
 N = 43; % Number of blades
 m_blade = 0.1; % kg, Mass of each blade
 r_blade = 22; % mm, blade length
+% r_0 = 
+% r_i = 
+h = 5; % mm, blade thickness
+
 %................
 
 omega = rev * 2*pi; %rad/s
 
-F = m_blade * omega^2 * r_blade; % N, force exerted by each blade on the disc
+F = m_blade * omega^2 * r_blade/1000; % N, force exerted by each blade
 fprintf('Force exerted by each blade on the disc: %1.0f kN\n', F/1000);
+
+
 
 
