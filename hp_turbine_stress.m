2a73cdecb1736e6a615beb1457faa150557bc662
OptimusPrime <optimusprime@87uu7.home>
2019-03-10 13:08:24 +0000

    found mass of blade

--- hp_turbine_stress.m
+++ hp_turbine_stress.m
@@ -2,17 +2,21 @@ clc
 % Define Variables.........
 rev = 431.39; % rev/s
 N = 43; % Number of blades
-m_blade = 0.1; % kg, Mass of each blade
-r_blade = 22; % mm, blade length
+r_blade = 22/1000; % m, blade length
 % r_0 = 
 % r_i = 
-h = 5; % mm, blade thickness
+h = 5/1000; % m, blade thickness
+width = 22/1000; %m, blade width
+rho = 8193; % kg/m3, density of high strength nickel-chromium alloy
 
 %................
 
 omega = rev * 2*pi; %rad/s
 
-F = m_blade * omega^2 * r_blade/1000; % N, force exerted by each blade
+m_blade = rho * h * r_blade * width; % kg, mass of individual blade
+fprintf('Mass of one blade: %1.2f kg\n',m_blade);
+
+F = m_blade * omega^2 * r_blade; % N, force exerted by each blade
 fprintf('Force exerted by each blade on the disc: %1.0f kN\n', F/1000);
 
 
